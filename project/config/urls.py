6f555081068cbf2ff0bf12ff243a97665abd3e6b
Cristian Paganno <simicro@gmail.com>
2024-04-25 19:55:38 -0300

    add: core

--- project/config/urls.py
+++ project/config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("cursos.urls")),
+    path("", include("core.urls")),
+    path("cursos/", include("cursos.urls")),
 ]
--- project/cursos/views.py
+++ project/cursos/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 from . import models
 
 def home(request):
-    return render(request, "cursos/index.html")
+    query = models.Comision.objects.all()
+    context = {"comisiones" : query}
+    return render(request, "cursos/index.html", context)
